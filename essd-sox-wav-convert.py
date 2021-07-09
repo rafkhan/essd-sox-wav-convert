e0e2c2d900621ceb1f5a234d837afb26cd734834
Rafael Khan <raf.khan.f@gmail.com>
2021-07-09 10:56:02 -0400

    fix srcdir prefix thing

--- essd-sox-wav-convert.py
+++ essd-sox-wav-convert.py
@@ -6,31 +6,33 @@ from pathlib import Path
 # https://www.threetom.com/news/batch-conversion-of-samples-for-erica-sample-drum/
 # sox -G input.wav --norm=-1 -b 16 -r 48k -c 1 output.wav
 
-def sox_convert_file(root, filename):
+def sox_convert_file(root, filename, srcdir_prefix):
   # os.
-  print(root + '/' + filename)
+  print(root[srcdir_prefix:] + '/' + filename)
+
+def get_srcdir_prefix_index(srcdir):
+    srcdir = os.path.abspath(srcdir)
+    return len(srcdir) + len(os.path.sep)
 
 # https://stackoverflow.com/questions/15663695/shutil-copytree-without-files
-def create_empty_dirtree(srcdir, dstdir, onerror=None):
-    srcdir = os.path.abspath(srcdir)
-    srcdir_prefix = len(srcdir) + len(os.path.sep)
-    os.makedirs(dstdir, exist_ok=True)
-    for root, dirs, files in os.walk(srcdir, onerror=onerror):
-        for dirname in dirs:
-            dirpath = os.path.join(dstdir, root[srcdir_prefix:], dirname)
-            try:
-                os.mkdir(dirpath)
-            except OSError as e:
-                if onerror is not None:
-                    onerror(e)
+def create_empty_dirtree(srcdir, srcdir_prefix, dstdir, onerror=None):
+  for root, dirs, _ in os.walk(srcdir, onerror=onerror):
+    for dirname in dirs:
+      dirpath = os.path.join(dstdir, root[srcdir_prefix:], dirname)
+      try:
+        os.mkdir(dirpath)
+      except OSError as e:
+        if onerror is not None:
+          onerror(e)
 
 def convert_input_directory(inputpath, outputpath):
-  create_empty_dirtree(inputpath, outputpath)
+  srcdir_prefix = get_srcdir_prefix_index(inputpath)
+  create_empty_dirtree(inputpath, srcdir_prefix, outputpath)
 
   for root, _, f_names in os.walk(inputpath):
     for filename in f_names:
       if filename.endswith(('.wav', '.mp3', '.aiff')):
-        sox_convert_file(root, filename)
+        sox_convert_file(root, filename, srcdir_prefix)
 
 def main():
   inputpath = ''
