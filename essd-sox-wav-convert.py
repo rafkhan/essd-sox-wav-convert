053679ec1ebe846fc15fba08d48220fd761a21fe
Rafael Khan <raf.khan.f@gmail.com>
2021-07-09 10:47:56 -0400

    create empty output tree

--- essd-sox-wav-convert.py
+++ essd-sox-wav-convert.py
@@ -3,11 +3,31 @@ import getopt
 import os
 from pathlib import Path
 
+# https://www.threetom.com/news/batch-conversion-of-samples-for-erica-sample-drum/
+# sox -G input.wav --norm=-1 -b 16 -r 48k -c 1 output.wav
+
 def sox_convert_file(root, filename):
+  # os.
   print(root + '/' + filename)
 
-def convert_input_directory(path):
-  for root, _, f_names in os.walk(path):
+# https://stackoverflow.com/questions/15663695/shutil-copytree-without-files
+def create_empty_dirtree(srcdir, dstdir, onerror=None):
+    srcdir = os.path.abspath(srcdir)
+    srcdir_prefix = len(srcdir) + len(os.path.sep)
+    os.makedirs(dstdir, exist_ok=True)
+    for root, dirs, files in os.walk(srcdir, onerror=onerror):
+        for dirname in dirs:
+            dirpath = os.path.join(dstdir, root[srcdir_prefix:], dirname)
+            try:
+                os.mkdir(dirpath)
+            except OSError as e:
+                if onerror is not None:
+                    onerror(e)
+
+def convert_input_directory(inputpath, outputpath):
+  create_empty_dirtree(inputpath, outputpath)
+
+  for root, _, f_names in os.walk(inputpath):
     for filename in f_names:
       if filename.endswith(('.wav', '.mp3', '.aiff')):
         sox_convert_file(root, filename)
@@ -35,7 +55,7 @@ def main():
 
   print('Reading from', inputpath)
   print('Writing to', outputpath)
-  convert_input_directory(inputpath)
+  convert_input_directory(inputpath, outputpath)
 
 if __name__ == "__main__":
    main()
